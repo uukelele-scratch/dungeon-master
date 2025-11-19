8445a4dfec78077073968ee07d37a5aabbad806f
uukelele-scratch <robustrobot11@gmail.com>
2025-11-19 07:30:11 +0000

    set desktop file name for application

--- src/main.py
+++ src/main.py
@@ -143,6 +143,7 @@ class MainWindow(QMainWindow):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    app.setDesktopFileName("dev.uukelele.dungeonmaster")
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
